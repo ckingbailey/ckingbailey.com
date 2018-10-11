319798b2b4a8a5d4db70c4d12b6df27f54bd56d4
ckb <ckingbailey@gmail.com>
2018-10-10 18:43:27 -0700

    install metalsmith layouts and markdown, render a sample

---
+++ index.js
@@ -0,0 +1,22 @@
+const metalsmith = require('metalsmith')
+const markdown = require('metalsmith-markdown')
+const layouts = require('metalsmith-layouts')
+
+metalsmith(__dirname)
+    .metadata({
+        title: "ckb - front-end super genius",
+        siteUrl: "http://ckingbailey.com",
+        author: "Colin King-Bailey",
+        description: "Colin King-Bailey's portfolio and resume",
+        keywords: "ckb web-developer portfolio resume javascript css html react expressjs nodejs mern-stack php mysql lamp-stack",
+        generatorname: "Metalsmith",
+        generatorurl: "http://metalsmith.io/"
+    })
+    .source('./src')
+    .destination('./build')
+    .clean(true)
+    .use(markdown())
+    .use(layouts())
+    .build(err => {
+        if (err) throw err
+    })
---
+++ src/hero.md
@@ -0,0 +1,9 @@
+---
+layout: index.njk
+headline: Web Developer
+---
+I create software that helps people get things done better
+
+In addition to static marketing pages, I've created a full-stack web app for managing data related to the end phase of infrastructure construction projects.
+
+Let's work together.
