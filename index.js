f485976f2898823f4ac156e4dbe5f16324373978
ckb <ckingbailey@gmail.com>
2018-10-20 15:10:23 -0700

    hero image

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const metalsmith = require('metalsmith')
 const markdown = require('metalsmith-markdown')
 const layouts = require('metalsmith-layouts')
-const static = require('metalsmith-static')
 const ignore = require('metalsmith-ignore')
 
 metalsmith(__dirname)
