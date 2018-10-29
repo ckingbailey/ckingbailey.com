3f5d8c7307d972f1395f3b58316215aa6587776b
ckb <ckingbailey@gmail.com>
2018-10-29 10:58:54 -0700

    close button for project modal

--- layouts/includes/script.js
+++ layouts/includes/script.js
@@ -2,19 +2,23 @@
 // possibly, but what I would have to do is make the click handler first call lookForBgImg
 // and then on success it calls the rest of the handler
 const checkParentForBgImg = (target) => {
-    console.log(target)
     if (target.tagName.toUpperCase() === "BODY") return null
     const parent = target.parentElement
     return parent.style.backgroundImage || checkParentForBgImg(parent)
 }
 
-const handleClick = ev => {
+const handleProjectCardClick = ev => {
     ev.preventDefault()
     const projectModal = document.getElementById('projectModal')
-    // look up the tree for backgroundImage
+    /* look up the tree for backgroundImage */
     const bgImg = ev.target.style.backgroundImage || checkParentForBgImg(ev.target)
-    console.log(bgImg)
     projectModal.style.display = 'block'
     projectModal.style.backgroundImage = `${bgImg}`
-    console.log(projectModal)
+}
+
+const handleCloseModalClick = ev => {
+    ev.preventDefault()
+    const projectModal = document.getElementById('projectModal')
+    projectModal.style.display = 'none'
+    projectModal.style.backgroundImage = ''
 }
