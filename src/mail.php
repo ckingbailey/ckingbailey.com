547c387f50d405b5c1613d82d67fc5b177ff0669
ckb <ckingbailey@gmail.com>
2018-11-01 17:41:14 -0700

    return Bad Request for anything other than POST

--- src/mail.php
+++ src/mail.php
@@ -1,9 +1,12 @@
 <?php
 use Mailgun\Mailgun;
 
-// require '../config.php';
 require '../vendor/autoload.php';
 
+if ($_SERVER['REQUEST_METHOD'] !== $_POST) {
+    header('Bad request, naughty naughty', true, 400);
+    exit;
+}
 if (empty($_POST)) {
     header('Location: ./index.html');
     exit;
@@ -22,14 +25,9 @@ $result = $mailer->sendMessage($domain, [
     'text' => "{$cleanPost['name']} [{$cleanPost['email']}] says:\r\n\r\n{$cleanPost['message']}\r\n\r\n{$cleanPost['name']}\r\n{$cleanPost['email']}\r\n{$cleanPost['phone']}"
 ]);
 
-if (!$result) {
-    echo "
-        <div style='margin: 3rem 3rem; font-family: monospace; color: red'>
-            <h1>Message send failed</h1>
-            <ul>
-                <li>$domain</li>
-                <li>" . MAILGUN_API_KEY . "</li>
-                <li>$mailgunUser</li>
-            </ul>
-        </div>";
-} else echo "<h1 style='color: green;'>Message sent</h1>";
+list($status, $body) = $result
+    ? [ 200, 'Thank you for your message. You can expect to hear from me soon.' ]
+    : [ 500, 'There was a problem sending your message.' ];
+
+header("Status: $status");
+echo $body;
