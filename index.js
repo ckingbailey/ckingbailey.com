5c1c6a44f26a4098856d2d1c8d357b0ebb924997
Colin <ckingbailey@gmail.com>
2018-11-02 21:37:38 -0700

    rebuild website to @jennerfins design using metalsmith static site generator (#33)
    * npm init and install metalsmith
    
    * add keywords "portfolio" and "resume" to package.json
    
    * install metalsmith layouts and markdown, render a sample
    
    * use | safe filter to prevent escaping line breaks
    
    * squirrel away dev files in _dev folder, old version in _old_version
    
    * make includes for various page pieces
    
    * ignore _dev folder and add terms to keywords
    
    * fix spelling of _dev* in gitignore
    
    * ignore public/
    
    * install metalsmith-static to build static files to public/
    
    * install metalsmith-ignore to ignore _dev folder
    
    * uninstall metalsmith-static coz it did not do what I thought it did
    
    * hero image
    
    * create nav bar
    
    * change dir structure to match ms-in-place example
    
    * add thimblecss and montserrat webfont (#15)
    
    * add splash content and style (#16)
    
    * add colors from mock up as css vars (#17)
    
    * rm test line from end of index.js that was breaking build (#18)
    
    * add colors from mock up
    
    * rm test line from end of indexjs
    
    * fix nav width so nav doesn't horizontally overflow page (#21)
    
    use w = 100% instead of 100vw for nav
    
    * add subtle grey background to splash element (#22)
    
    add subtly grey background to splash
    
    * add projects area to page (#23)
    
    * add project content
    
    * add Baskerville font and simplify how fonts are assigned to headings
    
    * layout project cards per design mock
    
    * add project images and transparent black overlay
    
    * add Contact section (#25)
    
    * add initial contact content
    
    * place form controls
    
    * style form controls
    
    * add another grey and make input ui respond to focus
    
    * css transition and narrower container for contact elements
    
    * playing around with form input box shadow, border, padding
    
    * trying some project image transparency & darkening techniques
    
    * click a project thumb to see full screen image of project (#27)
    
    * hover grows project thumbs
    
    * click handler to pop open a project modal
    
    * close button for project modal
    
    * make project images solid and move all imgs into img folder
    
    * make hover state show project name
    
    * onClick shows project screenshot
    
    * resize svbx and turt screencaps to 1600
    
    * use <img> for project full img instead of div w/ background
    
    * make close btn smaller and restore focus color
    
    * focus and active styles for control elements (#29)
    
    * restore outline and/or border + box-shadow to focused elements
    
    * active style for send button
    
    * add bg grey and bottom padding to body as stand-in for footer (#30)
    
    * wire up mail form (#31)
    
    * begin work on php mailer class and endpoint
    
    * wire up mailgun
    
    * send posted form data via mailgun
    
    * return Bad Request for anything other than POST
    
    * wire up a fetch request to mail.php
    
    * show success/failure modal when user submits new message (#32)
    
    * begin work on a success/failure contact modal
    
    * fix modal colors, rm dev stuff from script

---
+++ index.js
@@ -0,0 +1,38 @@
+const metalsmith = require('metalsmith')
+const msIf = require('metalsmith-if')
+const ignore = require('metalsmith-ignore')
+const inplace = require('metalsmith-in-place')
+const markdown = require('metalsmith-markdown')
+const watch = require('metalsmith-watch')
+
+const args = process.argv.slice(2)
+
+metalsmith(__dirname)
+    .metadata({
+        title: "ckb - front-end super genius",
+        siteUrl: "http://ckingbailey.com",
+        author: "Colin King-Bailey",
+        description: "Colin King-Bailey's portfolio and resume",
+        keywords: "ckb web-developer portfolio resume javascript css html react expressjs nodejs mern-stack php mysql lamp-stack",
+        generatorname: "Metalsmith",
+        generatorurl: "http://metalsmith.io/"
+    })
+    .source('./src')
+    .destination('./public')
+    .clean(true)
+    .use(markdown())
+    .use(inplace())
+    .use(ignore([ "**/_dev/*", "../**/layouts/includes" ]))
+    .use(msIf(
+        args[0] === "watch",
+        watch({
+            paths: {
+                "${source}/**/*": "**/*",
+                "layouts/**/*": "**/*"
+            },
+            livereload: args[0] === "watch"
+        })
+    ))
+    .build(err => {
+        if (err) throw err
+    })
---
+++ layouts/includes/script.js
@@ -0,0 +1,52 @@
+const checkParentForBgImg = (target) => {
+    if (target.tagName.toUpperCase() === "BODY") return null
+    const parent = target.parentElement
+    return parent.style.backgroundImage || checkParentForBgImg(parent)
+}
+
+const handleProjectCardClick = (ev, projectName) => {
+    ev.preventDefault()
+    const projectModal = document.getElementById('projectModal')
+    const projectImg = document.getElementById('projectImg')
+    projectModal.style.display = 'block'
+    projectImg.src = `./assets/img/${projectName}_screenshot.png`
+}
+
+const handleCloseModalClick = ev => {
+    ev.preventDefault()
+    const projectModal = document.getElementById('projectModal')
+    const projectImg = document.getElementById('projectImg')
+    projectModal.style.display = 'none'
+    projectImg.src = ''
+}
+
+const handleSubmit = ev => {
+    ev.preventDefault()
+    const formData = new FormData(ev.target)
+
+    fetch('mail.php', {
+        method: 'POST',
+        body: formData
+    })
+    .then((res, rej) => res.text())
+    .then (text => {
+        const message = text === 'Message sent.'
+            ? 'Thanks for your message. You\'ll be hearing from me soon.'
+            : 'I\'m sorry, there was a problem sending your message.'
+        showModal(message)
+    });
+}
+
+const showModal = text => {
+    const form = document.forms['mailForm']
+    const modal = document.getElementById('contactModal')
+    const msgContainer = modal.children['contactResMsg']
+
+    Array.from(form.elements).forEach(el => {
+        el.setAttribute('disabled', '')
+    })
+
+    msgContainer.innerText = text
+    modal.classList.add('success')
+    modal.style.display = 'flex'
+}
