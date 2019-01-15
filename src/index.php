f4b5b6b063636a545052a97dc784f0879fff659f
ckb <ckingbailey@gmail.com>
2019-01-14 17:56:26 -0800

    change CONSTs to env vars

--- src/index.php
+++ src/index.php
@@ -1,7 +1,7 @@
 <?php
 require '../vendor/autoload.php';
 session_start();
-$_SESSION['token'] = crypt($_SERVER['HTTP_USER_AGENT'], SALT);
+$_SESSION['token'] = crypt($_SERVER['HTTP_USER_AGENT'], getenv('SALT'));
 
 readfile('./index.html');
 exit;
