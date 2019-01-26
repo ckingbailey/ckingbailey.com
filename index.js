ebe1ac7b7944a92610e266a2ede6f302fd75add5
ckb <ckingbailey@gmail.com>
2019-01-25 18:37:36 -0800

    start to fix nav, hero, and splash for mobile

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const args = process.argv.slice(2)
 
 metalsmith(__dirname)
     .metadata({
-        title: "ckb - front-end super genius",
+        title: "CKB - Software doula",
         siteUrl: "http://ckingbailey.com",
         author: "Colin King-Bailey",
         description: "Colin King-Bailey's portfolio and resume",
